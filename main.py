ffa47d9ca34415c9f02655bf590faed09fc151a3
Taksexe <71042186+Taksexe@users.noreply.github.com>
2021-06-03 17:03:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+  
 import discord
 import os
 from keep_alive import keep_alive
 from replit import db
 import time
 import settings
+import random
 
 client = discord.Client()
 
@@ -18,10 +20,27 @@ async def on_message(message):
   if(message.author == client.user):
     return
   msg = message.content
-  if(msg.startswith('?play ')):
-    await message.channel.send('Takkinin çöptürük botundan bir ukte:')
+  if(msg.startswith('?play ege')):
+    await message.channel.send('İşten yeni çıktım arkadaşlar kendimi gangıster gibi hissediyorum')
+  if msg.startswith('?play orekto'):
+    await message.channel.send('███░███░███░███░░███░█░█░████\n█░░░█░░░█░█░█░░█░█░░░█░█░░░█░\n███░███░█░█░███░░███░█░█░░█░░\n█░░░█░░░█░█░█░░█░░░█░█░█░█░░░\n███░█░░░███░█░░█░███░███░████')
   if(msg.startswith("-p ") or msg.startswith("-play ")):
     await message.channel.send("playsene lan")
+
+
+#____________________dick part____________________#
+  if(msg.startswith("dick size") or msg.startswith("-play ")):
+    
+    await message.channel.send("playsene lan")
+    dickSize=random.random(0,20)
+    dick = "8"
+    while(dickSize!=0):
+      dick + "=" 
+    dick + ">"
+    await message.channel.send(dick)
+
+
+
     
   
 
